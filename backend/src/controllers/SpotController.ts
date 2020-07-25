a0f34770b9d1f74bc90bfc7a4d7c14fb1500b35a
gustavosimon <gustavo.andre.simon@hotmail.com>
2020-07-25 19:02:35 -0300

    Finished the backend of the application

--- backend/src/controllers/SpotController.ts
+++ backend/src/controllers/SpotController.ts
@@ -1,10 +1,30 @@
 import Spot from "../models/Spot";
+import User from "../models/User";
 import { Request, Response } from "express";
 
 class SpotController {
+  async index(req: Request, res: Response) {
+    const { tech } = req.query;
+    const spots = await Spot.find({ techs: tech });
+    return res.json(spots);
+  }
+
   async store(req: Request, res: Response) {
-    return res.json({ message: "ok" });
+    const { filename } = req.file;
+    const { company, techs, price } = req.body;
+    const { user_id } = req.headers;
+    const user = await User.findById(user_id);
+    if (!user) {
+      return res.status(400).json("User does not exists");
+    }
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      techs: techs.split(",").map((tech: String) => tech.trim()),
+      price,
+    });
+    return res.json(spot);
   }
 }
-
 export default SpotController;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,26 @@
 import express from "express";
+import multer from "multer";
+import uploadConfig from "./config/upload";
 import SessionController from "./controllers/SessionController";
 import SpotController from "./controllers/SpotController";
+import DashboardController from "./controllers/DashboardController";
+import BookingController from "./controllers/BookingController";
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 const sessionController = new SessionController();
 const spotsController = new SpotController();
+const dashboardController = new DashboardController();
+const bookingController = new BookingController();
 
 routes.post("/sessions", sessionController.store);
-routes.post("/spots", spotsController.store);
+
+routes.post("/spots", upload.single("thumbnail"), spotsController.store);
+routes.get("/spots", spotsController.index);
+
+routes.get("/dashboard", dashboardController.show);
+
+routes.post("/spots/:spot_id/bookings", bookingController.store);
 
 export default routes;
