1e34d1be4c28e31b83370fee5e4683e7bfa8615e
gustavosimon <gustavo.andre.simon@hotmail.com>
2020-07-28 21:58:15 -0300

    Developed the initial page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import api from "./services/api";
 import "./App.css";
+import logo from "./assets/logo.svg";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <img src={logo} alt="AirCnC" />
+
+          <div className="content">
+            <p>
+              Ofereça <strong>spots</strong> para programadores e encontre{" "}
+              <strong>talentos</strong> para sua empresa
+            </p>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="email">E-MAIL *</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Seu melhor email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </form>
+            <button type="submit" className="btn">
+              Entrar
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
